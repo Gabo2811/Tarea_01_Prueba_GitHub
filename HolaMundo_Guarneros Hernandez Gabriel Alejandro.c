c0ca74c9b4f8c58414975b3e14b5f722736f8462
Gabo2811 <79481596+Gabo2811@users.noreply.github.com>
2021-02-26 13:37:47 -0600

    Add files via upload

---
+++ HolaMundo_Guarneros Hernandez Gabriel Alejandro.c
@@ -0,0 +1,13 @@
+// 2AV3 Guarneros Hernández Gabriel Alejandro
+// Volumen de cono
+
+
+#include<stdio.h>
+
+
+int main()
+{
+    printf("Hola Gabriel Alejandro Guarneros Hernandez");
+
+   return 0;
+}
